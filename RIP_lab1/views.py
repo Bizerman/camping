4b7e4fe0757c09bac35527c228ccb1a9261a12a0
Bizarreman <lesha-utenkov@rambler.ru>
2024-10-07 17:16:22 +0300

    add files

--- RIP_lab1/views.py
+++ RIP_lab1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-def MainPage_Render(request):
-    return render(request, 'index.html')
+def main_page_render(request):
+    return render(request, 'products.html')
+def product_page_render(request):
+    return render(request, 'product.html', {'data' : {'products' : [
+        {'title': 'Gateway cosmonaut figure', 'id': 1},
+        {'title': 'Gateway mission patch', 'id': 2},
+        {'title': 'Gateway mission T-shirt', 'id': 3},
+    ]}})
