26dc5ad7c35ac7c4e7d5e78386d3df59b488b96d
Bizarreman <lesha-utenkov@rambler.ru>
2024-10-14 02:47:38 +0300

    add files

--- RIP_lab1/views.py
+++ RIP_lab1/views.py
@@ -1,22 +1,55 @@
 from django.shortcuts import render
-products = [{'title': 'Gateway cosmonaut figure', 'price': 3000, 'id': 1,
-             'info':'• Масштаб 1:6\n'
-                    ' • 13,5 дюйма в высоту с основанием\n'
-                    ' • 1400 грамм'
+products = [{'id': 1,
+             'title': 'Cygnus',
+             'description': 'Автономный грузовой космический корабль одноразового использования.',
+             'image':'http://127.0.0.1:9000/img-for-rip/images/',
+             'info':'Cygnus состоит из двух основных компонентов: сервисного модуля и грузового отсека. Сервисный модуль содержит двигатели, топливные баки, систему управления и другие системы, необходимые для управления полётом корабля. Грузовой отсек предназначен для перевозки различных грузов, таких как продукты питания, научное оборудование, материалы для экспериментов и т. д.\n'
+                    'Корабль запускается с помощью ракеты-носителя Antares, которая выводит его на орбиту. После отделения от ракеты-носителя Cygnus использует свои двигатели для корректировки орбиты и сближения с Международной космической станцией. Стыковка с МКС осуществляется с помощью манипулятора Canadarm2, который захватывает корабль и подводит его к стыковочному узлу станции.\n'
+                    'После разгрузки груза Cygnus может быть использован для проведения экспериментов или для возвращения грузов с МКС на Землю. Для этого корабль отделяется от станции и возвращается в атмосферу Земли, где его грузовой отсек сгорает, а оставшиеся части падают в океан.\n'
              },
-        {'title': 'Gateway mission patch', 'price': 400,'id': 2,'info':''},
-        {'title': 'Gateway mission T-shirt','price': 1500, 'id': 3,
-         'info' : 'Размеры - S-XXL\n'
-                  'Состав - Хлопок 60% / Полиэстр 40%'},]
-orders = []
-selected_products = []
-for product in products:
-    selected_products.append(product)
-orders.append({'id': 1, 'products': selected_products,'quantity': len(selected_products)})
+            {'id': 2,
+            'title': 'Power and Propulsion Element (PPE)',
+             'description': 'Модуль будет обеспечивать станцию Gateway энергией и управлять её двигательной установкой.',
+             'image':'http://127.0.0.1:9000/img-for-rip/images/',
+             'info':''},
+            {'id': 3,
+             'title': 'Dragon Crew Spacecraft',
+             'description': 'Автономный космический корабль, предназначенный для доставки экипажа и критически важных грузов на орбиту.',
+             'image':'http://127.0.0.1:9000/img-for-rip/images/',
+             'info' : 'Размеры - S-XXL\n'
+                      'Состав - Хлопок 60% / Полиэстр 40%'},
+            {'id': 4,
+             'title': 'Habitation and Logistics Outpost (HALO)',
+             'description': 'Жилой модуль , который обеспечит астронавтов рабочим пространством для исследования Луны и проживания на станции',
+             'image':'http://127.0.0.1:9000/img-for-rip/images/',
+             'info': 'Размеры - S-XXL\n'
+                     'Состав - Хлопок 60% / Полиэстр 40%'},
+            {'id': 5,
+             'title': 'Starliner Crew Spacecraft',
+             'description': 'Коммерческий космический корабль компании Boeing для перевозки людей.',
+             'image':'http://127.0.0.1:9000/img-for-rip/images/',
+             'info' : 'Размеры - S-XXL\n'
+                      'Состав - Хлопок 60% / Полиэстр 40%'},
+            {'id': 6,
+             'title': 'Dragon Cargo Spacecraft',
+             'description': 'Доставляет грузы на МКС и способен возвращать грузы обратно на Землю.',
+             'image':'http://127.0.0.1:9000/img-for-rip/images/',
+             'info' : 'Размеры - S-XXL\n'
+                      'Состав - Хлопок 60% / Полиэстр 40%'},]
+orders = [{'id': 1,
+               'products': [{'id': 2,'title': 'Power and Propulsion Element (PPE)',
+             'description': 'Модуль будет обеспечивать станцию Gateway энергией и управлять её двигательной установкой.',
+             'image':'http://127.0.0.1:9000/img-for-rip/images/',
+             'info':''},{'id': 4,'title': 'Habitation and Logistics Outpost (HALO)',
+             'description': 'Жилой модуль , который обеспечит астронавтов рабочим пространством для исследования Луны и проживания на станции',
+             'image':'http://127.0.0.1:9000/img-for-rip/images/',
+             'info': 'Размеры - S-XXL\n'
+                     'Состав - Хлопок 60% / Полиэстр 40%'}],
+               'quantity': 2}]
 
 def main_page_render(request):
     order = next((item for item in orders if item['id'] == 1), None)
-    search_query = request.GET.get('searchInput', '')
+    search_query = request.GET.get('gateway_el', '')
     filtered_products = [
         product for product in products
         if search_query in product['title']
@@ -27,5 +60,5 @@ def product_page_render(request,id):
     return render(request, 'product.html',{'product': product})
 def cart_page_render(request, id):
     order = next((item for item in orders if item['id'] == id), None)
-    return render(request, 'cart.html', {'order': order})
+    return render(request, 'orders.html', {'order': order})
 
--- bmstu/urls.py
+++ bmstu/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_page_render, name='products_url'),
     path('products/<int:id>/', views.product_page_render, name='product_url'),
-    path('cart/<int:id>/', views.cart_page_render, name='cart_url')
+    path('order/<int:id>/', views.cart_page_render, name='orders_url')
 ]
